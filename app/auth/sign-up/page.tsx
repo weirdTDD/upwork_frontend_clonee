f3e867dfe2e3c8c59d65f292469859281667cd54
Edward Morhe <edwardmorhe777@gmail.com>
2025-12-04 05:20:56 +0000

    form-02

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,8 +1,50 @@
+"use client"
+
 import { Navbar } from "@/components/sign-up-navbar/page"
+import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react";
+
+interface FormData {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    country: string;
+}
 
 
-const page = () => {
+
+
+const Page: React.FC = () => {
+
+   const [formData, setFormData] = useState<FormData>({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        country: "",
+   });
+
+   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value, type } = e.target;
+
+        const newValue = type === "checkbox" ? (e.target as HTMLInputElement).checked :value;
+
+        setFormData((prev) =>({
+            ...prev,
+            [name]: newValue,
+        }));
+   };
+
+   const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted", formData)
+   }
+
+
+
   return (
     <div> 
             
@@ -11,29 +53,67 @@ const page = () => {
                  <Navbar />
             </div>
 
-            <div className="py-20">
+            <div className="py-20 max-w-3xl mx-auto">
                
 
-                <div className="flex flex-col items-center gap-8 mt-32">
-                    <h2>Sign up to find work you love</h2>
+                <div className="flex flex-col items-center gap-8 mt-16">
+                    <h2 className="text-4xl font-semibold mb-4">Sign up to find work you love</h2>
 
-                    <div>
+                    <div className=" flex items-center gap-6">
                         <Link  href=""
-                            className= "border-2 border-gray-600 px-6 py-2 rounded-xl font-normal hover:scale-105 transition-transform"
+                            className= "flex items-center gap-6 border-2 border-gray-600 px-12 py-2 rounded-3xl font-normal hover:scale-105 transition-transform"
                         >
+                            <Image
+                             src="https://pngimg.com/uploads/apple_logo/small/apple_logo_PNG19683.png"
+                             alt=""
+                             width={15}
+                             height={15}
+                             className="inline-block"
+                            
+                            />
+
                             Continue with Apple
                         
                         </Link>
 
                         <Link  href=""
-                            className= "border-2 border-gray-600 px-6 py-2 rounded-xl font-normal hover:scale-105 transition-transform"
+                            className= "bg-blue-500 flex items-center gap-6 justify-between border-2 border-gray-600 px-0.5 py-1 rounded-3xl font-normal hover:scale-105 transition-transform"
                         >
-                            Continue with Google
+                            <Image
+                             src="https://cdn.iconscout.com/icon/free/png-256/free-google-icon-svg-download-png-1912023.png?f=webp&w=128"
+                             alt=""
+                             width={35}
+                             height={35}
+                             className=" rounded-full inline-block"
+                            
+                            />
+
+                            <p className="px-8 text-white">Continue with Google</p>
                         
                         </Link>
 
+                        
+
                     </div>
 
+                    <hr className="border border-gray-200 backdrop-blur-2xl w-full mx-auto px-16" />
+
+                </div>
+                <div>
+
+                    <form action="">
+                        <div className="flex grid-cols-2 items-center mx-auto gap-4">
+                            <div>
+                              <label htmlFor="text">First name</label>
+                              <input type="text" name="firstName" placeholder='First Name' value={formData.firstName} onChange={handleChange}  className='w-full p-3 border rounded-md focus:ring-2 hover:ring-blue-700' />  
+                            </div>
+                            <div>
+                              <label htmlFor="text">Last name</label>
+                              <input type="text" name="lastName" placeholder='Last name' value={formData.lastName} onChange={handleChange}  className='w-full p-3 border rounded-md focus:ring-2 hover:ring-blue-700' />  
+                            </div>
+                            
+                        </div>
+                    </form>
                 </div>
                 
             </div>
@@ -43,4 +123,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    domains:[
+      "pngimg.com",
+      "seekvectors.com",
+      "cdn.iconscout.com"
+      
+    ]
+  }
 };
 
 export default nextConfig;
